eb9aee2231a640743dd9cb8eb8596f4688802c11
Keelan Chu For <kkchufor@gmail.com>
2013-04-23 15:11:03 -0400

    Final commit
    Changed arch_layer to window.layer.
    Changed animation_layer to arch_layer.
    Removed old commented-out code.

--- src/sworcery.c
+++ src/sworcery.c
@@ -24,21 +24,11 @@ PBL_APP_INFO(MY_UUID,
 
 Window window;
 Layer arch_layer;
-Layer animation_layer;
 TextLayer text_debug_layer;
 TextLayer text_debug2_layer;
 TextLayer text_hobbit_layer;
 BmpContainer moon;
-BmpContainer bg_default;/*
-BmpContainer arch_norm;
-BmpContainer arch_turn;
-BmpContainer arch_smoke1;
-BmpContainer arch_smoke2;
-BmpContainer arch_smoke3;
-BmpContainer arch_smoke4;
-BmpContainer arch_smoke5;
-BmpContainer arch_smoke6;
-BmpContainer arch_raised;*/
+BmpContainer bg_default;
 BmpContainer arch_image;
 
 const int ARCH_IMAGE_RESOURCE_IDS[] = {
@@ -92,10 +82,15 @@ unsigned short get_display_hour(unsigned short hour) {
 
 //
 // Main image setting routine.
-// Used for every image swap in the app, the gears, the time digits, the day of week, the date
+// Used for every image swap in the app
 //
 
 void set_container_image(BmpContainer *bmp_container, const int resource_id, GPoint origin, Layer *targetLayer) {
+	/*
+	 Main image setting routine.
+	 Used for every image swap in the app
+	 Borrowed from https://github.com/xretsim/pebble_steampebble_mark1
+	 */
 	
 	layer_remove_from_parent(&bmp_container->layer.layer);            //remove it from layer so it can be safely deinited
 	bmp_deinit_container(bmp_container);                              //deinit the old image.
@@ -110,79 +105,6 @@ void set_container_image(BmpContainer *bmp_container, const int resource_id, GPo
 	layer_add_child(targetLayer, &bmp_container->layer.layer);        //add the new image to the target layer.
 }
 
-/*
-void animationlayer_update_callback(Layer *me, GContext* ctx) {
-	(void)me;
-	
-	PblTm t;
-	get_time(&t);
-	
-	// unused thus far
-	unsigned short display_hour = get_display_hour(t.tm_hour);
-	
-	
-	// sync animation with the time
-	if (t.tm_sec % SMOKE_LOOP == 0) {
-//	if (t.tm_sec == 0) {
-		animation_frame = 0;
-	}
-	*/
-/*	switch (animation_frame) {
-		case 1:
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke1.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 17:
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke2.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 3:
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke3.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 4:
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke4.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 5: case 16:
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke5.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 6: case 7: case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: 
-			graphics_draw_bitmap_in_rect(ctx, &arch_smoke6.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40: case 41: case 42:
-			graphics_draw_bitmap_in_rect(ctx, &arch_turn.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-		default:
-			graphics_draw_bitmap_in_rect(ctx, &arch_norm.bmp, GRect(86, 87, 144-100, 168-87));
-			break;
-	}
-*/
-/*	if (animation_frame == 1) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke1.bmp, ARCH_POS);
-	}
-	else if (animation_frame == 17) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke2.bmp, ARCH_POS);
-	}
-	else if (animation_frame == 3) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke3.bmp, ARCH_POS);
-	}
-	else if (animation_frame == 4) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke4.bmp, ARCH_POS);
-	}
-	else if (animation_frame == 5 || animation_frame == 16) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke5.bmp, ARCH_POS);
-	}
-	else if (animation_frame >= 6 && animation_frame <= 15) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_smoke6.bmp, ARCH_POS);
-	}
-	else if (animation_frame >= 33 && animation_frame <= 42) {
-		graphics_draw_bitmap_in_rect(ctx, &arch_turn.bmp, ARCH_POS);
-	}
-	else {
-		graphics_draw_bitmap_in_rect(ctx, &arch_norm.bmp, ARCH_POS);
-	}
-//	animation_frame = (animation_frame + 1) % FPERL;
-	animation_frame ++;
-	
-}*/
-
 
 void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
 	(void)ctx;
@@ -191,25 +113,25 @@ void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
 	if (cookie == SMOKE_TIMER) {
 		// animation sequence
 		if (animation_frame == 1) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[1], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[1], GPoint(86, 87), &arch_layer);
 		}
 		else if (animation_frame == 27) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[2], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[2], GPoint(86, 87), &arch_layer);
 		}
 		else if (animation_frame == 3) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[3], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[3], GPoint(86, 87), &arch_layer);
 		}
 		else if (animation_frame == 4) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[4], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[4], GPoint(86, 87), &arch_layer);
 		}
 		else if (animation_frame == 5 || animation_frame == 26) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[5], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[5], GPoint(86, 87), &arch_layer);
 		}
 		else if (animation_frame >= 6 && animation_frame <= 25) {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[6], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[6], GPoint(86, 87), &arch_layer);
 		}
 		else {
-			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[0], GPoint(86, 87), &animation_layer);
+			set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[0], GPoint(86, 87), &arch_layer);
 			
 			if (animation_frame >= 29) {
 				animation_frame = 0;
@@ -255,10 +177,10 @@ void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
 	
 	// arch_turn every half SMOKE_LOOP for 2 seconds
 	if ((display_second % SMOKE_LOOP) == SMOKE_LOOP/2) {
-		set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[7], GPoint(86, 87), &animation_layer);
+		set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[7], GPoint(86, 87), &arch_layer);
 	}
 	if ((display_second % SMOKE_LOOP) == SMOKE_LOOP/2 + 2) {
-		set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[0], GPoint(86, 87), &animation_layer);
+		set_container_image(&arch_image, ARCH_IMAGE_RESOURCE_IDS[0], GPoint(86, 87), &arch_layer);
 	}
 	
 	update_watchface(t->tick_time);
@@ -277,30 +199,16 @@ void handle_init(AppContextRef ctx) {
 	
 	// init the archetype layer
 	layer_init(&arch_layer, window.layer.frame);
-//	arch_layer.update_proc = &arch_layer_update_callback; // REF: .update_proc points to a function that draws the layer
 	layer_add_child(&window.layer, &arch_layer);
 	
 	
 	resource_init_current_app(&APP_RESOURCES);
 //	bmp_init_container(RESOURCE_ID_BG_DEFAULT, &bg_default);
 	bmp_init_container(RESOURCE_ID_MOON, &moon);
-/*	bmp_init_container(RESOURCE_ID_ARCH_NORM, &arch_norm);
-	bmp_init_container(RESOURCE_ID_ARCH_TURN, &arch_turn);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_ONE, &arch_smoke1);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_TWO, &arch_smoke2);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_THREE, &arch_smoke3);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_FOUR, &arch_smoke4);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_FIVE, &arch_smoke5);
-	bmp_init_container(RESOURCE_ID_ARCH_SMOKE_SIX, &arch_smoke6);
-	bmp_init_container(RESOURCE_ID_ARCH_RAISED, &arch_raised);*/
 	
 	// default background includes arch_norm
 //	layer_add_child(&arch_layer, &bg_default.layer.layer);
-	layer_add_child(&arch_layer, &moon.layer.layer);
-	
-	// animation stuff on arch_layer
-	layer_add_child(&arch_layer, &animation_layer);
-//	animation_layer.update_proc = &animationlayer_update_callback;
+	layer_add_child(&window.layer, &moon.layer.layer);
 	
 	// init the debug text layer
 	text_layer_init(&text_debug_layer, GRect(0, 0, 144, 30));
@@ -335,13 +243,7 @@ void handle_init(AppContextRef ctx) {
 
 void handle_deinit(AppContextRef ctx) {
 	(void)ctx;
-/*	
-	bmp_deinit_container(&arch_norm);
-	bmp_deinit_container(&arch_turn);
-	bmp_deinit_container(&arch_smoke1);
-	bmp_deinit_container(&arch_smoke2);
-	bmp_deinit_container(&arch_raised);
- */
+	
 	bmp_deinit_container(&arch_image);
 	bmp_deinit_container(&moon);
 	bmp_deinit_container(&bg_default);
